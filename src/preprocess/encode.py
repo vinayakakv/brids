61ecab75b2e7e1080459a7f2ea1ff02af5f1d858
vinayakakv <me.vinayakakv@gmail.com>
2019-12-27 14:26:14 +0530

    Fix bugs

--- src/preprocess/encode.py
+++ src/preprocess/encode.py
@@ -21,23 +21,26 @@ class Encoder:
         encoded_names = [f'{name}_{i}' for i in range(bits)]
         return encoded_names, encoded_values
 
-    def fit(self, x):
-        x = x.drop(['Dst Port', 'Protocol'], axis=1)
+    def fit(self, dataset):
+        x = dataset.drop(['Dst Port', 'Protocol', 'Label'], axis=1)
         self.names = x.columns
         x = np.log(2 + x.to_numpy(dtype='float32'), dtype='float32')
         self.standard_scalar.fit(x[:1])
         self.standard_scalar.std_ = x.std(0)
         self.standard_scalar.mean_ = x.mean(0)
 
-    def transform(self, x):
+    def transform(self, dataset):
+        y = dataset.pop('Label')
+        x = dataset      
         dst_port_names, dst_port_values = Encoder.__to_binary('Dst Port', 16, x['Dst Port'].values)
         protocol_names, protocol_values = Encoder.__to_binary('Protocol', 8, x['Protocol'].values)
-        x_t = x.drop(['Dst Port', 'Protocol'], axis=1)
-        x_t = np.log(2 + x_t.to_numpy(dtype='float32'))
+        x_t = x.drop(['Dst Port', 'Protocol'], axis=1).astype('float32')
+        x_t = np.log(2 + x_t)
         x_t = self.standard_scalar.transform(x_t)
         x_t = pd.DataFrame(x_t, columns=self.names)
         x_t[dst_port_names] = dst_port_values
         x_t[protocol_names] = protocol_values
+        x_t = x_t.assign(Label = y.values)
         return x_t
 
 
@@ -56,19 +59,9 @@ print("Reading files")
 try:
     train = pd.read_pickle(source_dir / "train.pickle")
     test = pd.read_pickle(source_dir / "test.pickle")
-    # x_train = pd.read_pickle(source_dir / "x_train.pickle")
-    # x_test = pd.read_pickle(source_dir / "x_test.pickle")
-    # y_train = pd.read_pickle(source_dir / "y_train.pickle")
-    # y_test = pd.read_pickle(source_dir / "y_test.pickle")
 except Exception as e:
     print(f"Error occured while reading files {e}")
     sys.exit(-1)
-
-train_labels = train['Label']
-test_labels = test['Label']
-
-train = train.loc[:, train.columns != 'Label']
-test = test.loc[:, test.columns != 'Label']
 
 e = Encoder()
 e.fit(train)
@@ -85,18 +78,11 @@ test = e.transform(test)
 labels = ["Benign","Bot","Brute Force -Web","Brute Force -XSS","DDOS attack-HOIC","DDOS attack-LOIC-UDP","DDoS attacks-LOIC-HTTP","DoS attacks-GoldenEye","DoS attacks-Hulk","DoS attacks-SlowHTTPTest","DoS attacks-Slowloris","FTP-BruteForce","Infilteration","SQL Injection","SSH-Bruteforce"]
 label_map = {label: i for i, label in enumerate(labels)}
 
-train['Label'] = train_labels.map(label_map, na_action='ignore')
-test['Label'] = test_labels.map(label_map, na_action='ignore')
+train['Label'] = train['Label'].map(label_map).astype('int8')
+test['Label'] = test['Label'].map(label_map).astype('int8')
 
 print("Saving encoded data")
 train.to_csv(dest_dir / "train.csv")
 test.to_csv(dest_dir/ "test.csv")
-# x_train.to_csv(dest_dir / "x_train.csv")
-# x_test.to_csv(dest_dir / "x_test.csv")
-# y_train.to_csv(dest_dir / "y_train.csv")
-# y_test.to_csv(dest_dir / "y_test.csv")
-# y_train = pd.get_dummies(y_train)
-# y_train.to_csv(dest_dir / "y_train_onehot.csv")
-# y_test = pd.get_dummies(y_test)
-# y_test.to_csv(dest_dir / "y_test_onehot.csv")
+
 print("Encoding DONE")
